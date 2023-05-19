c338509c7db9d17c09f6500d4a3ac239e5c30d1a
marwan-fahad <121671118+marwan-fahad@users.noreply.github.com>
2023-05-19 18:26:40 +0100

    Update Question.js

--- js/Question.js
+++ js/Question.js
@@ -46,11 +46,11 @@ class Question {
 
     this.button.mousePressed(() => {
 
-      // this.message.html("Thank You, Your Answer Has Been Submitted");
-      // this.message.position(350, 350);
+       this.message.html("Thank You, Your Answer Has Been Submitted");
+       this.message.position(350, 350);
 
-      this.message.html("Thank You, Your Answer Has Been Submitted");
-      this.message.position();
+      //this.message.html("Thank You, Your Answer Has Been Submitted");
+      //this.message.position();
 
       // this.message("Thank You, Your Answer Has Been Submitted");
       // this.message(350, 350);
